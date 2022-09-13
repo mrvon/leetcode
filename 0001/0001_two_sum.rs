cb6b4acd30bc34ab2ce2b6d18eb7d7e32c297276
dennis <mrvon@qq.com>
2022-09-13 19:55:03 +0800

    accepted

---
+++ 0001/0001_two_sum.rs
@@ -0,0 +1,20 @@
+enum Solution {}
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        use std::collections::HashMap;
+
+        let mut m: HashMap<i32, i32> = HashMap::new();
+        for (i, v) in nums.iter().enumerate() {
+            match m.get(&(target - *v)) {
+                Some(i2) => return vec![i as i32, *i2],
+                None => m.insert(*v, i as i32),
+            };
+        }
+        vec![]
+    }
+}
+
+fn main() {
+    println!("{:?}", Solution::two_sum(vec![2, 7, 11, 15], 9));
+}
